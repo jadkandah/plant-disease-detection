c5367b77fe1c19c9c7e329d9eb3578dbc0299e8a
faisal87-prog <faisalquntar87@gmail.com>
2026-03-22 00:00:05 +0300

    latest backend faisal

--- Application/backend/diseases/models.py
+++ Application/backend/diseases/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Disease(models.Model):
     plant = models.CharField(max_length=100)
-    disease = models.CharField(max_length=150)
+    disease = models.CharField(max_length=150, blank=True)
     is_healthy = models.BooleanField(default=False)
 
     def __str__(self):
--- Application/backend/diseases/views.py
+++ Application/backend/diseases/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .models import Disease
+from .serializers import DiseaseSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def disease_list(request):
+    diseases = Disease.objects.all()
+    serializer = DiseaseSerializer(diseases, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def disease_detail(request, pk):
+    try:
+        disease = Disease.objects.get(pk=pk)
+    except Disease.DoesNotExist:
+        return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = DiseaseSerializer(disease)
+    return Response(serializer.data)
