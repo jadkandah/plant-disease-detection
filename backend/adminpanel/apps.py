04ff3444627561ea9c115374123ca984b3b988a3
sanadf <sanadmadani@gmail.com>
2026-05-11 21:42:51 +0300

    a

--- backend/adminpanel/apps.py
+++ backend/adminpanel/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class AdminpanelConfig(AppConfig):
     name = 'adminpanel'
+
+# This tells Django:
+# "There is an app called adminpanel."
+# Django uses this file to recognize the app.
+# Nothing complicated here.
